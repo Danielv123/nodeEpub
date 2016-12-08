f10a176e7ebea894c9ffc0926504da2ad8861ae4
Danielv123 <tranen.bananen@gmail.com>
2016-12-08 12:00:10 +0100

    Increased max book size

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var bodyParser = require("body-parser");
 var app = express();
 
 app.use(express.static('books'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '500mb'}));
+app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 
 app.post("/epubify", function(req, res) {
 	res.header("Access-Control-Allow-Origin", "*");
